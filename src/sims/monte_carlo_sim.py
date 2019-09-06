a8b590ab58061cc1867c5e97b8692b88bc74c3ad
Andrew Parlane <andrew.parlane@gmail.com>
2019-09-06 15:08:28 -0300

    Moved the simulation and analysis of results to it's own function.

--- src/sims/monte_carlo_sim.py
+++ src/sims/monte_carlo_sim.py
@@ -23,6 +23,53 @@ class Result:
     tp:         float
     widths:     List[float]
 
+def _get_tp(circuit, tech, put, sim_time, step_time):
+    # ======================
+    # Ejecutar la simulación
+    # ======================
+    simulator = circuit.simulator(temperature=27, nominal_temperature=27)
+    analysis = simulator.transient(end_time=sim_time, step_time=step_time)
+
+    # ===============
+    # Encontrar el Tp
+    # ===============
+    in_rises = 0
+    out_transitions = 0
+
+    # usamos la función zip() para construir un tuple así que
+    # podríamos iterar por los dos arrays al mismo tiempo
+    # v es la tensión del nodo in y t es el tiempo de la muestra
+
+    # Prhimero encontrar la transición de la entrada
+    for (v,t) in zip(analysis['in'], analysis['in'].abscissa):
+        if (float(v) >= (tech.VDD / 2)):
+            in_rises = t
+            #print("In rises past 50% at " + str(in_rises.convert_to_power(-12)))
+            break
+
+    # Después encontrar la trasición de la salida
+    for (v, t) in zip(analysis['out'], analysis['out'].abscissa):
+        # la salida será un flanco ascendente o descendente?
+        if (put.inverts()):
+            if (float(v) <= (tech.VDD / 2)):
+                out_transitions = t
+                #print("Out falls past 50% at " + str(out_transitions.convert_to_power(-12)))
+                break
+        else:
+            if (float(v) >= (tech.VDD / 2)):
+                out_transitions = t
+                #print("Out rises past 50% at " + str(out_transitions.convert_to_power(-12)))
+                break
+
+    # =====================
+    # Devolver el resultado
+    # =====================
+    if (out_transitions == 0):
+        # La duración de simulación no estuvo suficiente largo
+        return -1
+    else:
+        return float(out_transitions - in_rises)
+
 # tech          - La tecnologia usar
 # put           - Path Under Test (Ruta bajo prueba)
 # step_time     - El escalon máximo para usar en la simulación
@@ -86,73 +133,27 @@ def do_monte_carlo_sim(tech, put, step_time, num_sims, plot_result):
         if (l % 100 == 0):
             print("Ran " + str(l) + "/" + str(num_sims) + " (" + str((100.0 * l)/num_sims) + "%)")
 
-        # ======================
-        # Ejecutar la simulación
-        # ======================
-        simulator = circuit.simulator(temperature=27, nominal_temperature=27)
-        analysis = simulator.transient(end_time=sim_time, step_time=step_time)
+            tp = _get_tp(circuit, tech, put, sim_time, step_time)
 
-        # ===============
-        # Encontrar el Tp
-        # ===============
-        in_rises = 0
-        out_transitions = 0
-
-        # Puedes acceder cualquier nodo en tu netlist en dos formas:
-        # analysis['out'] o analysis.out. Unos nombres están keywords
-        # en SPICE, por ejemplo in. Así no puedes usar analysis.in.
-        # Puedes usar analysis.in_ pero sugiero que usamos analysis['in']
-        # para todos los casos.
-
-        # analysis['foobar'] es un array de las muestras
-        # analysis['foobar'].abscissa es un array de los tiempos
-        # así analysis['foobar'][519] es la muestra del nodo foobar
-        # al tiempo analysis['foobar'].abscissa[519]
-
-        # usamos la función zip() para construir un tuple así que
-        # podríamos iterar por los dos arrays al mismo tiempo
-        # v es la tensión del nodo in y t es el tiempo de la muestra
-        for (v,t) in zip(analysis['in'], analysis['in'].abscissa):
-            if (float(v) >= (tech.VDD / 2)):
-                in_rises = t
-                #print("In rises past 50% at " + str(in_rises.convert_to_power(-12)))
-                break
-
-        for (v, t) in zip(analysis['out'], analysis['out'].abscissa):
-            # nos interesa cuando la salida tiene un flanco ascendente o descendente?
-            if (put.inverts()):
-                if (float(v) <= (tech.VDD / 2)):
-                    out_transitions = t
-                    #print("Out falls past 50% at " + str(out_transitions.convert_to_power(-12)))
-                    break
+            if (tp < 0):
+                # La duración de simulación no estuvo suficiente largo
+                # Si vimos una transición antes, entonces es claro que esto no puede ser mejor.
+                # Pero si nunca vimos una transición antes, incrementamos la duración
+                if (not succesfull_run):
+                    sim_time += sim_time_step
+                    print("Out never transititons, increasing simulation time to " + str(sim_time))
             else:
-                if (float(v) >= (tech.VDD / 2)):
-                    out_transitions = t
-                    #print("Out rises past 50% at " + str(out_transitions.convert_to_power(-12)))
-                    break
+                succesfull_run = True
 
-        # =====================
-        # Analizar el resultado
-        # =====================
+                # Usamos estos anchos si el tiempo de propagación total es menor que el mejor.
+                # Nota: que pasa si el tiempo de propagación es 1fs menor que el mejor
+                #       pero la area es mucho peor?
 
-        if (out_transitions == 0):
-            # only increase simulation time if we've never seen a valid run
-            if (not succesfull_run):
-                sim_time += sim_time_step
-                print("Out never transititons, increasing simulation time to " + str(sim_time))
-        else:
-            tp = float(out_transitions - in_rises)
-            succesfull_run = True
-
-            # Usamos estos anchos si el tiempo de propagación total es menor que el mejor.
-            # Nota: que pasa si el tiempo de propagación es 1fs menor que el mejor
-            #       pero la area es mucho peor?
-
-            if (tp < best_result.tp):
-                best_result = Result(tp, put.get_widths())
-                # reducir la duración de la simulación a tp + 50ps
-                sim_time    = tp + 50e-12
-                #print("New best Tp: " + str(best_result.tp) + " Widths " + str(best_result.widths) + " total widths: " + str(sum(best_result.widths)) + " reducing simulation time to " + str(sim_time))
+                if (tp < best_result.tp):
+                    best_result = Result(tp, put.get_widths())
+                    # reducir la duración de la simulación a tp + 50ps
+                    sim_time    = tp + 50e-12
+                    #print("New best Tp: " + str(best_result.tp) + " Widths " + str(best_result.widths) + " total widths: " + str(sum(best_result.widths)) + " reducing simulation time to " + str(sim_time))
 
         # =====================
         # Actualizar los anchos
