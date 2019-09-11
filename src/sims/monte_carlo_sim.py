417f291ed437c0c35fe88a8a1c65998b6d12974f
Andrew Parlane <andrew.parlane@gmail.com>
2019-09-11 19:03:34 -0300

    Trying to use total width as a second heuristic.
    Running with a long step time causes many simulations to have the same
    final Tp. We need a way to narrow down the "best" result. So look for
    the best total width, which is proportional to area.

--- src/sims/monte_carlo_sim.py
+++ src/sims/monte_carlo_sim.py
@@ -22,6 +22,7 @@ from typing import List
 class Result:
     tp:         float
     widths:     List[float]
+    totalWidth: float
 
 def _get_tp(circuit, tech, put, sim_time, step_time):
     # ======================
@@ -87,14 +88,13 @@ def _get_tp(circuit, tech, put, sim_time, step_time):
 #   mcs.do_monte_carlo_sim(tech, put, 1e-9, 10000, True)
 
 def do_monte_carlo_sim(tech, put, step_time, num_sims, plot_result):
-    # ================================
-    # Encontrar la carpeta de liberias
-    # ================================
-    libraries_path = find_libraries()
+    # initialisar el generador alatoria usando el tiempo del sistema
+    random.seed()
 
-    # ====================================
-    # Generar el parte general del netlist
-    # ====================================
+    # ==================
+    # Generar el netlist
+    # ==================
+    libraries_path = find_libraries()
     circuit = Circuit("Monte_Carlo_Sim_"+type(put).__name__+"_"+tech.NAME)
     circuit.include(libraries_path + "/" + tech.LIB_NAME)   #.inclcude "foo.lib"
 
@@ -103,64 +103,88 @@ def do_monte_carlo_sim(tech, put, step_time, num_sims, plot_result):
     # Fuente de tensión de pulses que usamos como la entrada de la ruta
     vin = circuit.PulseVoltageSource("In", "In", circuit.gnd, initial_value=0, pulsed_value=tech.VDD, pulse_width=1e-9, period=2e-9, delay_time=10e-12, rise_time=20e-12, fall_time=20e-12)
 
-    # ===========================
     # La ruta que queremos probar
-    # ===========================
     put.add_to_circuit(circuit, 'Vdd', 'In', 'Out')
 
-    # muestra el netlist
-    print(str(circuit))
+    # ======================
+    # Hacer las simulaciones
+    # ======================
 
-    # initialisar el generador alatoria usando el tiempo del sistema
-    random.seed()
-
-    # ====================
-    # Transient Simulación
-    # ====================
-
-    # start with simulating for 100ps
+    # Comienza simulando por 100ps
     sim_time = 100e-12
+
+    ts = time.time();
+
     # if simulation times out without seeing the transition, increase sim time by ...
     sim_time_step = 100e-12
     # Once we've seen a succesfull transisition (sim time was long enough)
     # we reduce simulation time to best_tp + 50ps
     succesfull_run = False
 
-    best_result = Result(100.0, put.get_widths())
+    # Hay varios resultados con el mismo (o muy parecido) Tp
+    # Si una simulación da un Tp un poco peor (1%?) peor usa menos area, deberíamos
+    # usar esto resultado cómo el mejor.
+    TP_FLEX_PERCENT = 1.0
 
-    ts = time.time();
+    # Guardamos el mejor Tp encontrado (comenzamos con 100s, así que cualquier simulación va a mejorar)
+    best_tp = 100.0
+
+    # Y guardamos el resultado actual (tal vez no el mejor Tp, pero el mejor area y muy cerca el mejor Tp)
+    result = Result(best_tp, put.get_widths(), sum(put.get_widths()))
+
     for l in range(num_sims):
         if (l % 100 == 0):
             print("Ran " + str(l) + "/" + str(num_sims) + " (" + str((100.0 * l)/num_sims) + "%)")
 
-            tp = _get_tp(circuit, tech, put, sim_time, step_time)
+        widths = put.get_widths()
+        totalWidth = sum(widths)
 
-            if (tp < 0):
-                # La duración de simulación no estuvo suficiente largo
-                # Si vimos una transición antes, entonces es claro que esto no puede ser mejor.
-                # Pero si nunca vimos una transición antes, incrementamos la duración
-                if (not succesfull_run):
-                    sim_time += sim_time_step
-                    print("Out never transititons, increasing simulation time to " + str(sim_time))
+        tp = _get_tp(circuit, tech, put, sim_time, step_time)
+        if (tp < 0):
+            # La duración de simulación no estuvo suficiente largo
+            # Si vimos una transición antes, entonces es claro que esto no puede ser mejor.
+            # Pero si nunca vimos una transición antes, incrementamos la duración
+            if (not succesfull_run):
+                sim_time += sim_time_step
+                print("Out never transititons, increasing simulation time to " + str(sim_time))
+        else:
+            succesfull_run = True
+
+            print("tp: " + str(tp) + ", totalWidth: " + str(totalWidth))
+
+            # este resultado tiene menor tp que el corriente mejor?
+            if (tp < best_tp):
+                print("  New Best Tp")
+                best_tp = tp
+
+                # reducir la duración de la simulación a tp + 50ps
+                sim_time = tp + 50e-12
+
+                # Tenemos un nuevo mejor Tp. Solo actualizamos el mejor resultado si tiene un Tp
+                # más lento (más de TP_FLEX_PERCENT) de este resultado. O si esta simulación también
+                # tiene mejor area que el mejor resultado.
+
+                if ((result.tp > (best_tp * ((100 + TP_FLEX_PERCENT) / 100))) or
+                    (result.totalWidth > totalWidth)):
+                    print("  New best result")
+                    result = Result(tp, widths, totalWidth)
             else:
-                succesfull_run = True
 
-                # Usamos estos anchos si el tiempo de propagación total es menor que el mejor.
-                # Nota: que pasa si el tiempo de propagación es 1fs menor que el mejor
-                #       pero la area es mucho peor?
+                # Aunque este resultado no tiene el mejor Tp, si es adentro de 1% del mejor
+                # Y tiene mejor area, entonces usamos esto.
+                if ((tp <= (best_tp * ((100 + TP_FLEX_PERCENT) / 100))) and
+                    (totalWidth < width)):
+                    print("  New best result")
+                    result = Result(tp, widths, totalWidth)
 
-                if (tp < best_result.tp):
-                    best_result = Result(tp, put.get_widths())
-                    # reducir la duración de la simulación a tp + 50ps
-                    sim_time    = tp + 50e-12
-                    #print("New best Tp: " + str(best_result.tp) + " Widths " + str(best_result.widths) + " total widths: " + str(sum(best_result.widths)) + " reducing simulation time to " + str(sim_time))
+            #print("New best Tp: " + str(result.tp) + " Widths " + str(result.widths) + " total widths: " + str(sum(result.widths)) + " reducing simulation time to " + str(sim_time))
 
         # =====================
         # Actualizar los anchos
         # =====================
 
         # Usamos los mejores anchos que encontramos y cambiar uno (no el primero)
-        widths = best_result.widths
+        widths = result.widths
 
         # Generar un ancho aleatoriamente entre tech.W_MIN y put.get_max_width()
         width = random.uniform(tech.W_MIN, put.get_max_width())
@@ -172,29 +196,28 @@ def do_monte_carlo_sim(tech, put, step_time, num_sims, plot_result):
         # Hazlo
         put.set_widths(widths)
 
+    te = time.time();
+
     # ======================
     # Mostrar los resultados
     # ======================
-    te = time.time();
     print("Took " + str(te - ts) + "s to run " + str(num_sims) + " simulations");
-    print("Best Tp " + str(best_result.tp) + " Widths " + str(best_result.widths))
+    print("Best Tp " + str(result.tp) + " Widths " + str(result.widths))
 
-    # ====================================
-    # Escribir los resultados a un archivo
-    # ====================================
     if not os.path.exists("results/"):
         os.mkdir("results/")
     f = open("results/"+type(put).__name__+"_"+tech.NAME+".txt","a+")
-    f.write(datetime.now().strftime("%Y/%m/%d %H:%M:%S") + ": Step time " + str(step_time) + " Num sims " + str(num_sims) + " Best Tp " + str(best_result.tp) + " Widths " + str(best_result.widths) + "\n")
+    f.write(datetime.now().strftime("%Y/%m/%d %H:%M:%S") + ": Step time " + str(step_time) + " Num sims " + str(num_sims) + " Best Tp " + str(result.tp) + " Widths " + str(result.widths) + "\n")
     f.close()
 
-    # ==============================================
-    # Find optimal Tp as according to logical effort
-    # ==============================================
+    # ============================================
+    # Encontrar el Tp optimo usando logical effort
+    # ============================================
     optWidths = put.get_logical_effort_optimal_widths()
     if (optWidths == None):
         print("Optimal case not supported by this path")
     else:
+        print("Running test with optimal widths calculated via logical effort: " + str(optWidths))
         put.set_widths(optWidths)
         tp = _get_tp(circuit, tech, put, sim_time, step_time)
 
@@ -205,10 +228,10 @@ def do_monte_carlo_sim(tech, put, step_time, num_sims, plot_result):
 
     # ================================================
     # Finalmente simula una vez más con anchos mejores
-    # Y plotear el resultado
+    # que encontramos y plotear el resultado
     # ================================================
-    if (plot_result and (best_result.tp > 0.0) and (best_result.tp < 1.0)):
-        put.set_widths(best_result.widths)
+    if (plot_result and (result.tp > 0.0) and (result.tp < 1.0)):
+        put.set_widths(result.widths)
         simulator = circuit.simulator(temperature=27, nominal_temperature=27)
         analysis = simulator.transient(end_time=sim_time*1.5, step_time=step_time)
         put.plot(analysis, 'In', 'Out')
